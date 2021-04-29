ad7de0ccf72c26377574d3fc46dcef6c2526aaa7
Maria Reyes Retana <mariareyesretana94@gmail.com>
2021-04-29 15:40:16 +0100

    Initial data
    Initial data files of the family household survey

---
+++ Data Reading.R
@@ -0,0 +1,13 @@
+##### Code for reading preliminary database 
+
+##### Libraries #####
+
+library(tidyverse)
+library(foreign)
+library(haven)
+
+##### Import dataset #####
+
+base <- read_dta("Data/hh09dta_b3b/iiib_ats.dta")
+
+
